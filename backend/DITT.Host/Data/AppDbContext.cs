b7525eb4760b7feaf3e25f2c51e67272f9912e43
MH221B <22127131@student.hcmus.edu.vn>
2026-04-08 11:14:19 +0700

    fix: ensure base model creation is called in OnModelCreating method

--- backend/DITT.Host/Data/AppDbContext.cs
+++ backend/DITT.Host/Data/AppDbContext.cs
@@ -14,6 +14,8 @@ public class AppDbContext : IdentityDbContext<IdentityUser>
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+        
         modelBuilder.Entity<Tool>(entity =>
         {
             entity.HasKey(e => e.Id);
